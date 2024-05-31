96163a857c3b6dcf48be87f352e2eb4aaaa9f207
finbae <tkfkadlek001@gmail.com>
2024-06-01 04:13:15 +0900

    edit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,13 +18,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://repository.map.naver.com/archive/maven")
         maven("https://jitpack.io")
-//        maven("https://jitpack.io"){
-//            credentials {
-//                username = "Songwon"
-//                password = "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"
-//            }
-//
-//        }
     }
 }
 
