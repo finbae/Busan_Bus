eec0ec5cd96b23f5edd51030d3676220cbb79204
finbae <tkfkadlek001@gmail.com>
2024-06-01 15:12:01 +0900

    edit ver.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,21 +44,6 @@ android {
         }
     }
 
-//    afterEvaluate {
-//        publishing {
-  //          publications {
-    //            create<MavenPublication>("maven") {
-      //              from(components["release"])
-        //            groupId = "com.github.mint"
-          //          artifactId = "mint-android-app"
-            //        version = "0.0.1"
-              //  }
-           // }
-        //}}
-
-
-
-
 }
 
 dependencies {
@@ -72,15 +57,14 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.naver.maps:map-sdk:3.18.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation ("com.github.skydoves:powerspinner:1.2.6")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.skydoves:powerspinner:1.2.6")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("com.github.User:Repo:Version")
     implementation("com.github.smart-fun:XmlToJson:1.5.3")
     implementation("com.android.volley:volley:1.2.1")
 }
