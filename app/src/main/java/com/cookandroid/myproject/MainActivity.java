9493afd2e64b085e621c94bd21dc2d1f75572d2a
finbae <tkfkadlek001@gmail.com>
2024-06-03 21:34:20 +0900

    edit api2

--- app/src/main/java/com/cookandroid/myproject/MainActivity.java
+++ app/src/main/java/com/cookandroid/myproject/MainActivity.java
@@ -1,6 +1,5 @@
 package com.cookandroid.myproject;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -8,12 +7,8 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.Manifest;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -26,18 +21,27 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import fr.arnaudguyon.xmltojsonlib.XmlToJson;
 
@@ -50,19 +54,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     Context context;
     public static final String TAG = MainActivity.class.getSimpleName();
-    public EditText edit;
-    public Button send;
-    TextView status1;
 
     String key = "hWgYHZ6wSKK1RN4xdueRnFz%2FVA405Tx%2BS0EvdwNZlyUviUzbvd5Ram9Z33045GZzCmFZd0uLqwKuMizAdKE2hQ%3D%3D";
+
+    private ArrayList<BusStop> busStops = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this.getBaseContext();
-
-        initView();
 
         NaverMapSdk.getInstance(this).setClient(
                 new NaverMapSdk.NaverCloudPlatformClient("3e8n3zzsba"));
@@ -74,7 +75,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                // 위치가 변할 때마다 실행
                 Log.i("MY LOCATION", "위도 : " + location.getLatitude());
                 Log.i("MY LOCATION", "경도 : " + location.getLongitude());
 
@@ -83,23 +83,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         };
 
         checkLocationPermission();
+        // 초기 버스 데이터 불러오기
+        fetchBusStopData();
     }
 
-    private void initView() {
-        status1 = (TextView) findViewById(R.id.result);
-        edit = (EditText) findViewById(R.id.message);
-        send = (Button) findViewById(R.id.send);
-
-        //messsage send action
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!edit.getText().toString().isEmpty()){
-                    BusArriveTask(edit.getText().toString());
-                    edit.setText(" ");
-                }
-            }
-        });
+    private void fetchBusStopData() {
+        // 샘플 버스 정류장 ID로 초기 데이터를 불러옵니다.
+        BusArriveTask("YOUR_BUS_STOP_ID");
     }
 
     private void BusArriveTask(String search) {
@@ -107,12 +97,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         String bstopid = null;
         try {
-            bstopid = URLEncoder.encode(search,"UTF-8");
+            bstopid = URLEncoder.encode(search, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        String url = "http://apis.data.go.kr/6260000/BusanBIMS/stopArrByBstopid?serviceKey=" + key + "&bstopid=" + bstopid;
+        String url = "http://apis.data.go.kr/6260000/BusanBIMS/stopArrByBstopid?serviceKey=" + key + "&bstopid=" + bstopid + "";
         Log.d(TAG, "URL:" + url);
 
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -120,13 +110,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onResponse(String response) {
                 XMLtoJSONData(response);
             }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-
-                    }
-                });
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e(TAG, "Error fetching data: " + volleyError.getMessage());
+            }
+        });
         requestQueue.add(request);
     }
 
@@ -149,8 +138,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 for (int i = 0; i < array.length(); i++) {
                     JSONObject obj = array.getJSONObject(i);
                     String lineno = obj.optString("lineNo"); // 버스 번호
-                    String gpsx = obj.optString("gpsx"); // x좌표
-                    String gpsy = obj.optString("gpsy"); // y좌표
+                    double gpsx = obj.optDouble("gpsx"); // x좌표
+                    double gpsy = obj.optDouble("gpsy"); // y좌표
                     String min1 = obj.optString("min1"); // 남은 도착시간
 
                     Log.d(TAG, "Bus number (lineno): " + lineno);
@@ -158,70 +147,82 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Log.d(TAG, "Y coordinate (gpsy): " + gpsy);
                     Log.d(TAG, "Remaining arrival time (min1): " + min1);
 
-                    // Save the coordinates for proximity checking
-                    BusStop busStop = new BusStop(lineno, Double.parseDouble(gpsx), Double.parseDouble(gpsy), min1);
+                    BusStop busStop = new BusStop(lineno, gpsx, gpsy, min1);
                     busStops.add(busStop);
                 }
             } else {
-                handleResultCode(resultCode);
+                handleAPIError(resultCode);
             }
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private void handleResultCode(String resultCode) {
+    private void handleAPIError(String resultCode) {
+        int messageResId;
         switch (resultCode) {
             case "1":
-                Toast.makeText(context, "시스템 에러가 발생하였습니다", Toast.LENGTH_SHORT).show();
+                messageResId = R.string.system_error;
                 break;
             case "4":
-                Toast.makeText(context, "결과가 존재하지 않습니다", Toast.LENGTH_SHORT).show();
+                messageResId = R.string.no_result;
                 break;
             case "8":
-                Toast.makeText(context, "요청 제한을 초과하였습니다", Toast.LENGTH_SHORT).show();
+                messageResId = R.string.request_limit_exceeded;
                 break;
             case "23":
-                Toast.makeText(context, "버스 도착 정보가 존재하지 않습니다", Toast.LENGTH_SHORT).show();
+                messageResId = R.string.no_bus_arrival_info;
                 break;
+            default:
+                messageResId = R.string.unknown_error;
+                break;
+        }
+        Toast.makeText(context, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkProximityAndLog(Location location) {
+        for (BusStop busStop : busStops) {
+            float[] results = new float[1];
+            Location.distanceBetween(location.getLatitude(), location.getLongitude(), busStop.gpsY, busStop.gpsX, results);
+            if (results[0] < 100) { // 100 meters 이내일 경우
+                Log.i("Proximity Alert", "버스 번호: " + busStop.lineNo + ", 남은 도착시간: " + busStop.min1);
+            }
         }
     }
 
     private void checkLocationPermission() {
-        // 위치 권한 허용 여부 확인
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // 권한 요청
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    100);
+                    LOCATION_PERMISSION_REQUEST_CODE);
         } else {
-            // 권한이 허용되었을 때, 위치 업데이트 요청
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER,
                     3000,
-                    -1,
+                    1,
                     locationListener);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 100) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한이 허용되었을 때, 위치 업데이트 요청
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            3000,
-                            -1,
-                            locationListener);
-                }
+                startLocationUpdates();
+            } else {
+                Toast.makeText(this, "위치 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
             }
         }
         if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated()) { // 권한 거부됨
+            if (!locationSource.isActivated()) {
                 naverMap.setLocationTrackingMode(LocationTrackingMode.None);
             }
             return;
@@ -234,7 +235,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.naverMap = naverMap;
         naverMap.setLocationSource(locationSource);
 
-        // 위치 권한이 허용된 경우에만 LocationTrackingMode.Follow 설정
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
@@ -247,8 +247,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    // 새로운 클래스 추가: BusStop
-    private class BusStop {
+    static class BusStop {
         String lineNo;
         double gpsX;
         double gpsY;
@@ -259,18 +258,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             this.gpsX = gpsX;
             this.gpsY = gpsY;
             this.min1 = min1;
-        }
-    }
-
-    private ArrayList<BusStop> busStops = new ArrayList<>();
-
-    private void checkProximityAndLog(Location location) {
-        for (BusStop busStop : busStops) {
-            float[] results = new float[1];
-            Location.distanceBetween(location.getLatitude(), location.getLongitude(), busStop.gpsY, busStop.gpsX, results);
-            if (results[0] < 100) { // 100 meters proximity check
-                Log.i("PROXIMITY ALERT", "Bus number: " + busStop.lineNo + ", Remaining time: " + busStop.min1);
-            }
         }
     }
 }
